ba4b451b327f52088c58157fccbad2926f03638b
Eoin Stankard <G00300390@gmit.ie>
2020-12-19 13:59:45 +0000

    Updated server

--- Project-Shop/server.py
+++ Project-Shop/server.py
@@ -1,8 +1,8 @@
 from ProjectShop import projectShop
 from flask import Flask, url_for, request, redirect, abort, jsonify
-
+from flask_cors import CORS
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
-
+CORS(app)
 @app.route('/')
 def index():
     return "hello"
