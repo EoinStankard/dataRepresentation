169fd04f30a5b55358a84486a7cc6b2b2e95dfc0
Eoin Stankard <G00300390@gmit.ie>
2020-12-24 16:23:00 +0000

    Added line CORS(app)

--- Project-Shop/server.py
+++ Project-Shop/server.py
@@ -2,7 +2,7 @@ from ProjectShop import projectShop
 from flask import Flask, url_for, request, redirect, abort, jsonify
 from flask_cors import CORS
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
-#CORS(app)
+CORS(app)
 @app.route('/')
 def index():
     return "hello"
